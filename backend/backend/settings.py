3301c6f4acf641bb4a9d2415fa248c13e2cbc53e
Joe Pena <joempena26@gmail.com>
2020-05-04 22:36:51 -0600

    Add cors support

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api'
+    'api',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# CORS settings
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8080',
+    'https://cc-component.netlify.app'
+)
